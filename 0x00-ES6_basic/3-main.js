9a35402475778800ce56be84321ec90258e54f32
Amineafilal <amineafilalelalami@gmail.com>
2025-05-07 02:48:12 +0200

    3-default-parameter.js

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,8 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
+// Output: 142
+// Output: 56
+// Output: 41
