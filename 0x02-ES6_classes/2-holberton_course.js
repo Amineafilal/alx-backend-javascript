da7e3c8746384c15806da168a5a2b90be43dec8d
Amineafilal <amineafilalelalami@gmail.com>
2025-05-15 14:47:16 +0200

    22222222222

--- 0x02-ES6_classes/2-holberton_course.js
+++ 0x02-ES6_classes/2-holberton_course.js
@@ -1,8 +1,20 @@
+// File: 2-holberton_course.js
+
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
@@ -32,7 +44,7 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
